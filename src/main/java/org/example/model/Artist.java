21d430e412ae5981c1bdd69f54f360a78aaceac4
Vinicius <vinireis010305@gmail.com>
2023-03-22 18:14:35 -0300

    Alterando o Model de Artist

--- src/main/java/org/example/model/Artist.java
+++ src/main/java/org/example/model/Artist.java
@@ -1,4 +1,27 @@
 package org.example.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Artist")
 public class Artist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "artist_ID")
+    private long id;
+
+    @Column(name = "artist_name")
+    private String name;
+
+    @Column(name = "artist_type")
+    private String type;
+
 }
