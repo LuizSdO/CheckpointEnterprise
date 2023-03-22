74ea148a72b56c6f5b13b235fc6b1da1b632cbe5
Vinicius <vinireis010305@gmail.com>
2023-03-22 18:14:49 -0300

    Alterando o Model de RecordLabel

--- src/main/java/org/example/model/RecordLabel.java
+++ src/main/java/org/example/model/RecordLabel.java
@@ -1,4 +1,26 @@
 package org.example.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Record Label")
 public class RecordLabel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "record_label_ID")
+    private long id;
+
+    @Column(name = "record_label_name")
+    private String name;
+
+    private String website;
 }
