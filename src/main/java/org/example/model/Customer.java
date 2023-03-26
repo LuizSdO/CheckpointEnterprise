2515f9aee2072331c13f86436240e9db4fe8a7fb
gugitia <61124594+gugitia@users.noreply.github.com>
2023-03-26 11:48:53 -0300

    alteração da model Custumer

--- src/main/java/org/example/model/Customer.java
+++ src/main/java/org/example/model/Customer.java
@@ -1,4 +1,39 @@
 package org.example.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Costumer")
 public class Customer {
+        @Email
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "card_information")
+    private String info;
+
+    @Column(name = "ip_address")
+    private String ip;
+
+    @Column(name = "device")
+    private String device;
+
 }
