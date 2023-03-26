131164cacb41614597ec1a241550be5e1afc0e07
gugitia <61124594+gugitia@users.noreply.github.com>
2023-03-26 11:50:04 -0300

    Alteração da model Genre

--- src/main/java/org/example/model/Genre.java
+++ src/main/java/org/example/model/Genre.java
@@ -1,4 +1,25 @@
 package org.example.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+@Table(name = "Genre")
 public class Genre {
+       @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "genre_ID")
+    private long id;
+
+    @Column(name = "genre_name")
+    private String name;
 }
+
